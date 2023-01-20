67fb7956d83e7c50ea0dac010c1fc8b5fa4993ad
tijani abdellatif <tijani.idrissi.abdellatif@gmail.com>
2023-01-20 18:36:37 +0100

    starter routes

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,15 +1,13 @@
 import express from 'express';
 import {json} from 'body-parser';
+import { currentUserRouter } from './routes/current';
 
 const app = express();
 app.use(json());
+app.use(currentUserRouter);
 
-
-app.get('/api/users/current',(req,res) =>{
-        res.send('hi there')
-});
 
 app.listen(3000,() =>{
 
-      console.log('listen on 3000 jjhh');
+      console.log('listen on 3000');
 });
---
+++ auth/src/routes/current.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+const router = express.Router();
+router.get('/api/users/current',() => {
+
+    
+
+});
+
+export { router as currentUserRouter };
