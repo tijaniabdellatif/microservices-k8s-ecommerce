a5923cb64a55a68d499d5d0caac1c6c0bdda213c
tijani abdellatif <tijani.idrissi.abdellatif@gmail.com>
2023-01-20 18:53:06 +0100

    validation

--- auth/src/routes/register.ts
+++ auth/src/routes/register.ts
@@ -1,8 +1,24 @@
-import express from 'express';
+import express,{Request,Response} from 'express';
+import { body } from 'express-validator';
 const router = express.Router();
-router.post('/api/users/register',(req,res) => {
 
-    res.send('Hi wolrd');
+router.post('/api/users/register',[
+
+    body('email')
+    .isEmail()
+    .withMessage('Email must be valid'),
+
+    body('password')
+    .trim()
+    .isLength({min:5,max:20})
+    .withMessage('Password must be validated')
+
+   
+
+],(req: Request,res: Response) => {
+
+    const { email , pasword} = req.body;
+
 
 
 });
