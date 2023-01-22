34c508a83edfc68dd6642fc3dd8773ad9a9ec162
tijani abdellatif <tijani.idrissi.abdellatif@gmail.com>
2023-01-22 13:04:06 +0100

    handling not found error

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,12 +5,17 @@ import { userLogOut } from './routes/logout';
 import { userLogin } from './routes/login';
 import { userRegister } from './routes/register';
 import { errorHandler } from './middlewares/ErrorHandler';
+import { NotFound } from './errors/NotFound';
 const app = express();
 app.use(json());
 app.use(currentUserRouter);
 app.use(userLogin);
 app.use(userLogOut);
 app.use(userRegister);
+
+app.all('*',() =>{
+   throw new NotFound();
+})
 app.use(errorHandler);
 
 
