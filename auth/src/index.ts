2e7990179230bdd552178531ac8847e818ff74f1
tijani abdellatif <tijani.idrissi.abdellatif@gmail.com>
2023-01-22 15:41:41 +0100

    deploy and create service user model

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -8,6 +8,7 @@ import { userLogin } from './routes/login';
 import { userRegister } from './routes/register';
 import { errorHandler } from './middlewares/ErrorHandler';
 import { NotFound } from './errors/NotFound';
+
 const app = express();
 app.use(json());
 app.use(currentUserRouter);
@@ -18,9 +19,6 @@ app.use(userRegister);
 app.all('*',async (req,res,next) =>{
   next(new NotFound());
 })
-// app.all('*',async (req,res,next) =>{
-//       next(new NotFound());
-// });
 
 app.use(errorHandler);
 
@@ -29,7 +27,7 @@ const start = async () =>{
   try{
     await mongoose.connect('mongodb://auth-mongo-srv:27017/auth',{
       family: 4,
-  });
+    });
 
   console.log('connected to monggodb')
   }catch(error){
@@ -37,8 +35,7 @@ const start = async () =>{
       console.error(error);
   }
 
-  app.listen(3000,() =>{
-
+app.listen(3000,() =>{
     console.log('listen on 3000');
  });
  
---
+++ auth/src/models/User.ts
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+const userSchema = new mongoose.Schema({
+
+    name:{
+
+          type:String,
+          required:true
+    },
+
+    email : {
+
+         type:String,
+         required:true
+    },
+
+    password:{
+
+        type:String,
+        required:true
+    }
+
+});
+
+const User = mongoose.model('User',userSchema);
+export { User as UserModel };
