2a32e2228d7eb7bc6565f67bb1b3de2bb23a8727
tijani abdellatif <tijani.idrissi.abdellatif@gmail.com>
2023-01-23 13:44:55 +0100

    completing hash password manager

--- auth/src/services/PasswordHash.ts
+++ auth/src/services/PasswordHash.ts
@@ -5,13 +5,15 @@ const scryptAsync = promisify(scrypt);
 export class PasswordHash {
     static async toHash(password: string){
         const salt = randomBytes(8).toString('hex');
-        const buffer = (await scryptAsync(password,salt,64)) as Buffer ;  
-
+        const buffer = (await scryptAsync(password,salt,64)) as Buffer;  
         return `${buffer.toString('hex')}.${salt}`;
     }
 
-    static compare(storedPassword:string,suppliedPassword:string){
+    static async compare(storedPassword:string,suppliedPassword:string){
 
+          const [hashedPassword,salt] = storedPassword.split('.');
+          const buffer = (await scryptAsync(suppliedPassword,salt,64)) as Buffer; 
+          return buffer.toString('hex') === hashedPassword;
 
     }
 
