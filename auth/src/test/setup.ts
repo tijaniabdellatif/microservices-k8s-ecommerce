fea5a314a7158546945a56119c172178b16915f0
tijani abdellatif <tijani.idrissi.abdellatif@gmail.com>
2023-01-25 11:14:46 +0100

    setup test env and file

---
+++ auth/src/test/setup.ts
@@ -0,0 +1,30 @@
+import {MongoMemoryServer} from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+import {app} from '../app';
+
+let mongo: any;
+beforeAll(async () =>{
+    mongo = new MongoMemoryServer();
+    const mongoUri = await mongo.getUri();
+    await mongoose.connect(mongoUri,{
+
+        family:4
+    });
+
+});
+
+beforeEach(async() =>{
+
+    const collections = await mongoose.connection.db.collections();
+
+    for(let collection of collections){
+
+          await collection.deleteMany({})
+    }
+});
+
+afterAll( async () =>{
+
+      await mongo.stop();
+      await mongoose.connection.close();
+})
