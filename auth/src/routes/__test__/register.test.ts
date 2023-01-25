260effea427dd4e2eaccf28e3e674bcefd949dce
tijani abdellatif <tijani.idrissi.abdellatif@gmail.com>
2023-01-25 11:46:54 +0100

    testing invaliding inputs

--- auth/src/routes/__test__/register.test.ts
+++ auth/src/routes/__test__/register.test.ts
@@ -11,4 +11,65 @@ it('returns 201 on success',async () => {
           password:'qazxswedc12'
       }).expect(201);
       
-})
+});
+
+it('return 400 with an invalid email',async () =>{
+
+    return request(app)
+    .post('/api/users/register')
+    .send({
+        name:'test test',
+        email:'test@test',
+        password:'qazxswedc12'
+    }).expect(400);
+
+});
+
+it('return 400 with an invalid password',async () =>{
+
+    return request(app)
+    .post('/api/users/register')
+    .send({
+        name:'test test',
+        email:'test@test.ma',
+        password:'qazxswedc'
+    }).expect(400);
+
+});
+
+it('return 400 with an empty name',async () =>{
+
+    return request(app)
+    .post('/api/users/register')
+    .send({
+        name:'',
+        email:'test@test.ma',
+        password:'qazxswedc'
+    }).expect(400);
+
+});
+
+it('return 400 with missing email and password and name',async () =>{
+
+    await request(app)
+    .post('/api/users/register')
+    .send({
+       name:'test test'
+    }).expect(400);
+
+    await request(app)
+    .post('/api/users/register')
+    .send({
+       email:'tijani@tijani.ma'
+    }).expect(400);
+
+    await request(app)
+    .post('/api/users/register')
+    .send({
+       email:'test@test.ma',
+       name:'test test'
+    }).expect(400);
+
+});
+
+
