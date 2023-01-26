6c5784731590390dcb1ab1e5960f49c029e43520
tijani abdellatif <tijani.idrissi.abdellatif@gmail.com>
2023-01-26 23:32:11 +0100

    things must be refactored

---
+++ client/pages/_app.js
@@ -0,0 +1,9 @@
+import 'bootstrap/dist/css/bootstrap.css';
+/**
+ * Component for exporting automatically
+ * from pages folder
+ */
+export default ({Component,pageProps}) =>{
+    return <Component {...pageProps} />
+};
+
