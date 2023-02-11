c1f58cb4db21d65cb3f4b43eba740d97f6df0035
tijani abdellatif <tijani.idrissi.abdellatif@gmail.com>
2023-02-11 16:16:59 +0100

    Updates 🅿️

--- common/src/index.ts
+++ common/src/index.ts
@@ -3,12 +3,14 @@ interface Color {
     red:number;
     blue:number;
     green:number;
+    opacity:number,
 }
 const color:Color = {
 
       red:10,
       blue:10,
-      green:10
+      green:10,
+      opacity:0.3
 };
 
 
